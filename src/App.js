abd68cb917e9094e460cfd8c0211d1872537356a
tapasvikumarpatel <t.patel@outlook.de>
2023-07-31 12:27:25 +0200

    image compressor

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Resizer from 'react-image-file-resizer';
 
-function App() {
+export default function App() {
+  const [compressedImage, setCompressedImage] = useState(null);
+
+  const handleImageChange = async (e) => {
+    const file = e.target.files[0];
+
+    // Set the quality for the compressed image
+    const quality = 75;
+
+    // Compress the image using react-image-file-resizer
+    await Resizer.imageFileResizer(
+      file,
+      file.width, // Set the width to the original width of the image
+      file.height, // Set the height to the original height of the image
+      'JPEG',
+      quality,
+      0,
+      (uri) => {
+        setCompressedImage(uri);
+      },
+      'base64'
+    );
+  };
+
+  const handleDownloadClick = () => {
+    if (compressedImage) {
+      // Create a temporary anchor element to trigger file download
+      const downloadLink = document.createElement('a');
+      downloadLink.href = compressedImage;
+      downloadLink.download = 'compressed_image.jpg';
+      document.body.appendChild(downloadLink);
+      downloadLink.click();
+      document.body.removeChild(downloadLink);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* Input for image upload */}
+      <input type="file" accept="image/*" onChange={handleImageChange} />
+
+      {/* Display the original image (optional) */}
+      {compressedImage && <img src={compressedImage} alt="Compressed" />}
+
+      {/* Download button */}
+      {compressedImage && (
+        <button onClick={handleDownloadClick}>Download Compressed Image</button>
+      )}
     </div>
   );
-}
-
-export default App;
+};
